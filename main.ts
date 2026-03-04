eafe842145f1b96fb6d317ca22e6f3b3a4f91f46
Viktor Popp <ViktorPopp.dev@gmail.com>
2026-03-04 09:20:45 +0100

    better :D

--- main.ts
+++ main.ts
@@ -1,47 +1,54 @@
-let xPos: number = 0
+let player_pos = 0;
 let bullets: [number, number][] = []
 
-// ========
+//
 // Graphics
-// ========
+//
 
 function redraw() {
     basic.clearScreen()
-    led.toggle(xPos, 4)
+    led.plot(player_pos, 4)
     bullets.forEach((b) => {
         const [x, y] = b
-        led.toggle(x, y)
+        led.plot(x, y)
     })
 }
 
-// ============
-// Bullet Logic
-// ============
+//
+// Bullets
+//
 
 function spawnBullet() {
-    bullets.push([xPos, 3])
+    bullets.push([player_pos, 3])
 }
 
-function moveBullets() {
+function simulateBullets() {
     for (let i = 0; i < bullets.length; i++) {
-        let b = bullets[i];
-        let [x, y] = b
+        let [x, y] = bullets[i]
+        if (y == 0) {
+            bullets.removeAt(i)
+            i -= 1
+        }
         y -= 1
         bullets[i] = [x, y]
     }
 }
 
-// ==============
-// Input Handling
-// ==============
+//
+// Input Logic
+//
 
-input.onButtonPressed(Button.A, function () {
-    xPos -= 1
+input.onButtonPressed(Button.A, function() {
+    if (player_pos == 0)
+        return
+    player_pos -= 1
     redraw()
 })
 
 input.onButtonPressed(Button.B, function () {
-    xPos += 1
+    if (player_pos == 4)
+        return
+    player_pos += 1
     redraw()
 })
 
@@ -50,11 +57,15 @@ input.onButtonPressed(Button.AB, function () {
     redraw()
 })
 
+//
+// General
+//
+
 basic.clearScreen()
 redraw()
 
-basic.forever(function () {
-    moveBullets()
-    basic.pause(50)
+basic.forever(() => {
+    simulateBullets()
     redraw()
+    basic.pause(100)
 })
